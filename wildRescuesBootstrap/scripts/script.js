404f3f104586016bd1b44925885b6b503050c082
aurielleRosenberger <81722773+aurielleRosenberger@users.noreply.github.com>
2021-04-28 20:06:48 -0600

    Add files via upload

---
+++ wildRescuesBootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Auri Rosenberger
+    File Name: script.js
+    Date: 28, April 2021
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
