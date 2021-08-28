b724f4d327d8e555aca6809c593b9be1c6364572
aurielleRosenberger <81722773+aurielleRosenberger@users.noreply.github.com>
2021-08-27 23:23:42 -0600

    Add files via upload

---
+++ project1BusinessPestControl/week1Project1.js
@@ -0,0 +1,25 @@
+/* *******************************************************************
+***
+*Original Author: Auri Rosennberger
+*Date Created: 26, August 2021
+*Version: 1
+*Date Last Modified: 27, August 2021
+*Modified by: Auri Rosennberger
+*Modification log:
+
+- 08/27/2021 - Added alert for customer's free inspection
+***
+******************************************************************** */
+
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+/* let soliciteCoupon = prompt("Your first inspection is free! Would you like to get your second discounted? (y/n)");
+// yesCoupon = prompt(soliciteCoupon);
+if (soliciteCoupon == y)
+    alert("Great! Use the code J438DFH for your 25% discount!");
+else
+    alert("Okay! Maybe next time!"); */
+
+    alert("Got pests? Looking to find a reputable exterminator? We'll give you your first inspection for free!");
